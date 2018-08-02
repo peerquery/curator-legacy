3dd3a569fb69503dfb917137ccc0f668874a550c
dzivenu <makafuigdzivenu@gmail.com>
2018-08-02 17:55:30 +0000

    add route handler for *bot* and *FAQs* pages

--- routes/static.js
+++ routes/static.js
@@ -5,6 +5,14 @@ module.exports = function (server) {
 		res.render('static/index');
 	});
 	
+	server.get('/bot', function(req, res){ 
+		res.render('static/bot');
+	});
+	
+	server.get('/faqs', function(req, res){ 
+		res.render('static/faqs');
+	});
+	
 	server.get('/sponsors', function(req, res){ 
 		res.render('static/sponsors');
 	});
