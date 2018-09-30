97f46fe37a9e7fcd2f7bb796d85db0ea8e3c8442
dzivenu <makafuigdzivenu@gmail.com>
2018-09-30 14:06:03 +0000

    setup global jquery and init dropdown component

--- src/client/ui/scripts.js
+++ src/client/ui/scripts.js
@@ -1,2 +1,19 @@
 
+const $ = require('jquery');
+window.jQuery = $;
+window.$ = $;
+
 require('semantic-ui-css/semantic.min.js');
+
+
+$('.ui.menu .ui.dropdown').dropdown({
+    on: 'hover'
+});
+$('.ui.menu a.item')
+    .on('click', function() {
+        $(this)
+            .addClass('active')
+            .siblings()
+            .removeClass('active')
+        ;
+    });
