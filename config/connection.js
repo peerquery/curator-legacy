0409fb947e44fba9bfe788efe63c50c5b66a3c1c
dzivenu <makafuigdzivenu@gmail.com>
2018-10-01 15:27:47 +0000

    use connection string instead of object

--- config/connection.js
+++ config/connection.js
@@ -5,14 +5,7 @@ var util = require('util');
 var mysql = require('mysql');
 var dotvenv = require('dotenv').config();
 
-var pool = mysql.createPool({
-    connectionLimit: process.env.DB_CONNECTIONLIMIT,
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    multipleStatements: process.env.DB_MULTIPLESTATEMENETS
-});
+var pool = mysql.createPool(process.env.DB_STRING);
 
 pool.getConnection((err, connection) => {
     if (err) {
