6f612a73509522aac04e422443f6326e3dbbe1fd
dzivenu <makafuigdzivenu@gmail.com>
2018-10-02 10:25:04 +0000

    correct wrong naming of critical config

--- config/email.js
+++ config/email.js
@@ -7,7 +7,7 @@ var transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
     auth: {
-        user: process.env.PROJECT_EMAIL,
+        user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
     tls: {
